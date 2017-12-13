fb151d4999b1773159c475a7acd6ab4ecf6b7d1a
Roy <gao.luo@honjoinfo.com>
2017-12-13 18:14:53 +0800

    init react demo with parcel

---
+++ index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import './index.css'
+import './index.scss'
+class HelloMessage extends React.Component {
+    render() {
+        return (
+            <div><h1>Hello {this.props.name}</h1></div>
+        );
+    }
+}
+
+var mountNode = document.getElementById("app");
+ReactDOM.render(
+    <HelloMessage name="Jane"/>, mountNode);
